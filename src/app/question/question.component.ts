126d6abe55c4a259678ad42b27b8a62c679d82e7
amardeepSrivastav <srivastav.amar2711@gmail.com>
2022-02-23 03:40:38 -0500

    4 files modified

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -12,10 +12,44 @@ export class QuestionComponent implements OnInit {
     private router: Router,
     private questionService: QuestionService
   ) {}
-
+  qns: any[];
   ngOnInit() {
-    this.questionService.getQuestions().subscribe((data: any) => {
-      console.log(data);
-    });
+    if (parseInt(localStorage.getItem('seconds')) > 0) {
+      this.questionService.seconds = parseInt(localStorage.getItem('seconds'));
+      this.questionService.qnProgress = parseInt(
+        localStorage.getItem('qnProgress')
+      );
+      this.questionService.qns = JSON.parse(localStorage.getItem('qns'));
+      if (this.questionService.qnProgress == 8)
+        this.router.navigate(['/question']);
+      else this.startTimer();
+    } else {
+      this.questionService.seconds = 0;
+      this.questionService.qnProgress = 0;
+      this.questionService.getQuestions().subscribe((data: any) => {
+        this.questionService.qns = data;
+        this.startTimer();
+      });
+    }
+  }
+  startTimer() {
+    this.questionService.timer = setInterval(() => {
+      this.questionService.seconds++;
+      localStorage.setItem('seconds', this.questionService.seconds.toString());
+    }, 1000);
+  }
+
+  Answer(qID, choice) {
+    this.questionService.qns[this.questionService.qnProgress].answer = choice;
+    localStorage.setItem('qns', JSON.stringify(this.questionService.qns));
+    this.questionService.qnProgress++;
+    localStorage.setItem(
+      'qnProgress',
+      this.questionService.qnProgress.toString()
+    );
+    if (this.questionService.qnProgress == 10) {
+      clearInterval(this.questionService.timer);
+      this.router.navigate(['/result']);
+    }
   }
 }
--- src/app/shared/question.service.ts
+++ src/app/shared/question.service.ts
@@ -5,9 +5,19 @@ import { HttpClient } from '@angular/common/http';
 export class QuestionService {
   //---------------- Properties---------------
   readonly rootUrl = 'http://localhost:5000';
+  qns: any[];
+  seconds: number;
+  timer;
+  qnProgress: number;
+  correctAnswerCount: number = 0;
   constructor(private http: HttpClient) {}
 
   getQuestions() {
-    return this.http.get(this.rootUrl + '/api/Question');
+    return this.http.get(this.rootUrl + '/api/Questions');
+  }
+
+  getAnswers() {
+    var body = this.qns.map((x) => x.QnID);
+    return this.http.post(this.rootUrl + '/api/Answers', body);
   }
 }
