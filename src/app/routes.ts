c2adb75bdca62be642522b48da64b86778565d88
amardeepSrivastav <srivastav.amar2711@gmail.com>
2022-02-23 02:17:33 -0500

    1 file modified

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
 import { QuestionService } from './shared/question.service';
 
 export const appRoutes: Routes = [
-  { path: 'question', component: QuestionService },
+  { path: 'question', component: QuestionComponent },
   { path: '', redirectTo: '/question', pathMatch: 'full' },
 ];
